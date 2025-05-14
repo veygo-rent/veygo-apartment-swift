68ba72c3ca9cb0115bb325f9820ba4c2435f4076
Shenghong <me@zhoud2001.io>
2025-05-14 00:13:03 -0400

    add swift dependency

--- veygo-apartment-swift/ContentView.swift
+++ veygo-apartment-swift/ContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Stripe
 
 struct ContentView: View {
     var body: some View {
@@ -14,6 +15,34 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+                .onTapGesture {
+
+                    let cardParams = STPPaymentMethodCardParams()
+                    cardParams.number = "4242424242424242"
+                    cardParams.expMonth = NSNumber(value: 10)
+                    cardParams.expYear = NSNumber(value: 2028)
+                    cardParams.cvc = "374"
+
+                    let billingDetails = STPPaymentMethodBillingDetails()
+                    billingDetails.name = "John Appleseed"
+
+                    let paymentMethodParams = STPPaymentMethodParams(
+                        card: cardParams,
+                        billingDetails: billingDetails,
+                        metadata: nil
+                    )
+
+                    STPAPIClient.shared.createPaymentMethod(with: paymentMethodParams) { paymentMethod, error in
+                        if let error = error {
+                            print("Error creating payment method: \(error.localizedDescription)")
+                        } else if let paymentMethod = paymentMethod {
+                            print("Payment method created: \(paymentMethod.stripeId)")
+                            // Send this paymentMethod ID to your backend
+                        }
+                    }
+
+
+                }
         }
         .padding()
     }
--- veygo-apartment-swift/veygo_apartment_swift.swift
+++ veygo-apartment-swift/veygo_apartment_swift.swift
@@ -6,9 +6,13 @@
 //
 
 import SwiftUI
+import Stripe
 
 @main
 struct veygo_apartment_swift: App {
+    init() {
+            StripeAPI.defaultPublishableKey = "pk_live_51QzCjkL87NN9tQEdbASm7SXLCkcDPiwlEbBpOVQk5wZcjOPISrtTVFfK1SFKIlqyoksRIHusp5UcRYJLvZwkyK0a00kdPmuxhM"
+        }
     var body: some Scene {
         WindowGroup {
             ContentView()
