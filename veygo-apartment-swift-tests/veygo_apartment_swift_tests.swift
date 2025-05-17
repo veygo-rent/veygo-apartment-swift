8f5c693533d8af2c7748c793fdd73724da578111
Shenghong <me@zhoud2001.io>
2025-05-17 01:12:13 -0400

    init swift test

---
+++ veygo-apartment-swift-tests/veygo_apartment_swift_tests.swift
@@ -0,0 +1,16 @@
+//
+//  veygo_apartment_swift_tests.swift
+//  veygo-apartment-swift-tests
+//
+//  Created by Shenghong Zhou on 5/17/25.
+//
+
+import Testing
+
+struct veygo_apartment_swift_tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
