0e9519b9da08e8e96bd7ad34eb9933d0c212d22d
Xinyidududu <guan90@purdue.edu>
2025-05-18 14:41:53 -0400

    tool bar and main page draft done

---
+++ veygo-apartment-swift/Views/Plans.swift
@@ -0,0 +1,20 @@
+//
+//  Plans.swift
+//  veygo-apartment-swift
+//
+//  Created by 魔法玛丽大炮 on 5/18/25.
+//
+import SwiftUI
+
+struct Plans: View {
+    var body: some View {
+        Text("Plans Page")
+            .font(.largeTitle)
+            .foregroundColor(.blue)
+    }
+}
+
+#Preview {
+    Plans()
+}
+
---
+++ veygo-apartment-swift/Views/Rental.swift
@@ -0,0 +1,22 @@
+//
+//  Rental.swift
+//  veygo-apartment-swift
+//
+//  Created by 魔法玛丽大炮 on 5/18/25.
+//
+
+import SwiftUI
+
+struct Rental: View {
+    var body: some View {
+        Text("Rental Page")
+            .font(.largeTitle)
+            .foregroundColor(.blue)
+    }
+}
+
+#Preview {
+    Rental()
+}
+
+
