4b2bb4fff7a249af279177ae3f0783791cdbacdb
Shenghong <me@zhoud2001.io>
2025-06-29 03:26:51 -0400

    fine jpg it is

--- veygo-apartment-swift/Views/ContentView.swift
+++ veygo-apartment-swift/Views/ContentView.swift
@@ -69,7 +69,7 @@ struct ContentView: View {
                         NotificationManager.shared.requestPermission()
                         if !apns_token.isEmpty && apns_token != prev_apns_token {
                             prev_apns_token = apns_token
-                            var body: [String: String] = ["apns": apns_token]
+                            let body: [String: String] = ["apns": apns_token]
                             let jsonData = try? JSONSerialization.data(withJSONObject: body)
                             let update_apns_request = veygoCurlRequest(url: "/api/v1/user/update-apns", method: "POST", headers: ["auth": "\(token)$\(userId)"], body: jsonData)
                             URLSession.shared.dataTask(with: update_apns_request) { data, response, error in
