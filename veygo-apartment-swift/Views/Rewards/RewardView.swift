23aebb51836bac3b8d8ff2f0c162e72c1431bca3
sardine <129631747+Xinyidududu@users.noreply.github.com>
2025-07-14 21:30:29 -0400

    Move Plans to reward page just for now

--- veygo-apartment-swift/Views/Rewards/RewardView.swift
+++ veygo-apartment-swift/Views/Rewards/RewardView.swift
@@ -4,18 +4,71 @@
 //
 //  Created by Shenghong Zhou on 6/30/25.
 //
+
 import SwiftUI
 
-public struct RewardView: View {
-    public var body: some View {
-        Text("Reward Page")
-            .font(.largeTitle)
-            .foregroundColor(.blue)
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
+struct RewardView: View {
+    var body: some View {
+        VStack(spacing: 0) {
+            BannerView(title: "Plans", showTitle: true, showBackButton: false)
+                .ignoresSafeArea(.container, edges: .top)
+
+            ScrollView {
+                VStack(spacing: 24) {
+                    PlanCard(
+                        title: "Basic",
+                        subtitle: "Start saving, no strings attached.",
+                        price: "Free",
+                        billingCycle: "",
+                        features: [
+                            "Access to rent cars",
+                            "Free additional driver for rentals",
+                            "Free upgrade to premium cars",
+                            "10% discount on ALL rentals"
+                        ],
+                        isCurrentPlan: true,
+                        isFreePlan: true,
+                        onSwitch: {}
+                    )
+
+                    PlanCard(
+                        title: "Plus",
+                        subtitle: "More savings, lower fees.",
+                        price: "$10.99",
+                        billingCycle: "month",
+                        features: [
+                            "Access to rent cars",
+                            "Free additional driver for rentals",
+                            "Free upgrade to premium cars",
+                            "10% discount on ALL rentals"
+                        ],
+                        isCurrentPlan: false,
+                        onSwitch: { print("Switch to Plus") }
+                    )
+
+                    PlanCard(
+                        title: "Premier",
+                        subtitle: "The most premium tier with highest rewards.",
+                        price: "$24.99",
+                        billingCycle: "month",
+                        features: [
+                            "Access to rent cars",
+                            "Free additional driver for rentals",
+                            "Free upgrade to premium cars",
+                            "10% discount on ALL rentals"
+                        ],
+                        isCurrentPlan: false,
+                        onSwitch: { print("Switch to Premier") }
+                    )
+                }
+            }
+        }
+        .background(Color("MainBG"))
     }
 }
 
 #Preview {
     RewardView()
 }
+
 
