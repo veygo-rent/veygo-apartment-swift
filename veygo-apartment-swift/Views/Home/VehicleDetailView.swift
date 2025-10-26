9b36b8cd43e9c5205ce3616ec8408527105590eb
Danny Zhou <me@zhoud2001.io>
2025-10-26 02:43:50 -0400

    Update VehicleDetailView.swift

--- veygo-apartment-swift/Views/Home/VehicleDetailView.swift
+++ veygo-apartment-swift/Views/Home/VehicleDetailView.swift
@@ -25,26 +25,73 @@ struct VehicleDetailView: View {
     @State private var includePAI = false
     var body: some View {
         ScrollView {
-            ZStack {
-                Image("VehicleShowroom")
-                    .resizable()
-                    .aspectRatio(1.3, contentMode: .fill)
-                    .frame(maxWidth: .infinity)
-                    .clipped()
-                Image("TempVehicle")
-                    .resizable()
-                    .aspectRatio(1.25, contentMode: .fill)
-                    .scaleEffect(0.8, anchor: .bottom)
-            }
-            VStack(alignment: .leading, spacing: 20) {
-                if vehicleWithBlocksAndLocationInfo.0.isVehicleAvailable(start: startTime, end: endTime) {
-                    AvailableVehicle()
-                } else {
-                    UnavailableVehicle()
+            VStack(spacing: 0) {
+                ZStack {
+                    Image("VehicleShowroom")
+                        .resizable()
+                        .aspectRatio(1.3, contentMode: .fill)
+                        .frame(maxWidth: .infinity)
+                        .clipped()
+                    Image("TempVehicle")
+                        .resizable()
+                        .aspectRatio(1.25, contentMode: .fill)
+                        .scaleEffect(0.8, anchor: .bottom)
                 }
-                Spacer()
+                VStack(spacing: 0) {
+                    VStack(spacing: 18) {
+                        Text(vehicleWithBlocksAndLocationInfo.0.vehicle.name)
+                            .fontWeight(.bold)
+                            .font(.title)
+                            .foregroundStyle(Color("TextBlackPrimary"))
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                        Text("\(vehicleWithBlocksAndLocationInfo.0.vehicle.year) \(vehicleWithBlocksAndLocationInfo.0.vehicle.make) \(vehicleWithBlocksAndLocationInfo.0.vehicle.model)")
+                            .font(.callout)
+                            .foregroundStyle(Color("FootNote"))
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                        Grid(alignment: .leading, verticalSpacing: 10) {
+                            GridRow {
+                                Image(systemName: "person.fill")
+                                    .gridColumnAlignment(.center)
+                                Text("5 People")
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                                    .padding(.leading, 4)
+                                Image(systemName: "car.top.arrowtriangle.rear.right.fill")
+                                    .gridColumnAlignment(.center)
+                                Text("4 Doors")
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                                    .padding(.leading, 4)
+                            }
+                            GridRow {
+                                Image(systemName: "suitcase.fill")
+                                Text("2 Small Bags")
+                                    .padding(.leading, 4)
+                                Image(systemName: "figure.walk.suitcase.rolling.circle.fill")
+                                Text("2 Big Bags")
+                                    .padding(.leading, 4)
+                            }
+                            GridRow {
+                                Image(systemName: "location.app.fill")
+                                Text("In Car Nav")
+                                    .padding(.leading, 4)
+                                Image(systemName: "car.rear.road.lane")
+                                Text("Lane Keep")
+                                    .padding(.leading, 4)
+                            }
+                        }
+                        .font(.subheadline)
+                        .foregroundStyle(Color("TextBlackSecondary"))
+                    }
+                    .frame(maxWidth: .infinity)
+                    .padding(18)
+                    .background(Color("CardBG"))
+                    if vehicleWithBlocksAndLocationInfo.0.isVehicleAvailable(start: startTime, end: endTime) {
+                        AvailableVehicle()
+                    } else {
+                        UnavailableVehicle()
+                    }
+                    Spacer()
+                }
             }
-            .padding(.horizontal, 24)
             .frame(maxWidth: .infinity, alignment: .leading)
         }
         .background(Color("MainBG"))
