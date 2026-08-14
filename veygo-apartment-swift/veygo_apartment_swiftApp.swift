bde1a46a5e1ceff9bba79b8f0132212ef6af1605
Danny Zhou <me@zhoud2001.io>
2026-08-13 21:26:19 -0400

    Initial Commit

---
+++ veygo-apartment-swift/veygo_apartment_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  veygo_apartment_swiftApp.swift
+//  veygo-apartment-swift
+//
+//  Created by Shenghong Zhou on 8/13/26.
+//
+
+import SwiftUI
+
+@main
+struct veygo_apartment_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
